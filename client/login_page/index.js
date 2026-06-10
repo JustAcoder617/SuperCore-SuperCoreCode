ec304d7f5a6331430f2b52c5ce6374653288c8d3
Alysson <antonioalvesdasilvaalysson0@Gmail.com>
2026-06-10 10:00:44 -0300

    feat: implementar autenticação de usuário com bcrypt e ajustes na conexão com o banco de dados

--- client/login_page/index.js
+++ client/login_page/index.js
@@ -1,6 +1,83 @@
 const env_btn=document.getElementById("env");
 const username_input=document.getElementById("usr");
 const password_input=document.getElementById("psw");
+const sleep = (ms) => {
+  return new Promise(resolve => setTimeout(resolve, ms));
+};
+function getCookie(nome) {
+  const cookies = document.cookie.split("; ");
+
+  for (let cookie of cookies) {
+    const [chave, valor] = cookie.split("=");
+    if (chave === nome) {
+      return decodeURIComponent(valor);
+    }
+  }
+
+  return null;
+}
+
+function set_button(estado, user, password){
+    if(estado=="1"){
+        env_btn.style.boxShadow="5px 5px 5px green";
+        env_btn.innerText="Login Realizado com sucesso!";
+        await sleep(5000);
+        env_btn.style="";
+        set_logged_login();
+        window.location.href="../index.html"
+    }
+    if(estado=="2"){
+        env_btn.style.boxShadow="5px 5px 5px yellow";
+        env_btn.style.backgroundColor="yellow"
+        env_btn.innerText="Usuário ou Senha Incorretos!";
+        await sleep(5000);
+        env_btn.style="";
+    }
+}
+
+
+async function set_logged_login(user, password){
+    let pass;
+    try{
+        const data={
+            content: password
+        }
+        let response = await fetch('/get_hash', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        });
+    }catch(err){
+
+    }
+    document.cookie(`user=${user}; psw=${pass}; max-age=259.200; path=/; Secure; SameSite=Strict;`)
+}
+
+
+
+function set_button(estado, user, password){
+    if(estado=="1"){
+        env_btn.style.boxShadow="5px 5px 5px green";
+        env_btn.innerText="Login Realizado com sucesso!";
+        await sleep(5000);
+        env_btn.style="";
+        set_logged_login();
+        window.location.href="../index.html"
+    }
+    if(estado=="2"){
+        env_btn.style.boxShadow="5px 5px 5px yellow";
+        env_btn.style.backgroundColor="yellow"
+        env_btn.innerText="Usuário ou Senha Incorretos!";
+        await sleep(5000);
+        env_btn.style="";
+    }
+}
 async function enviar_dados_ao_sv(data) {
-    
+    try{
+
+    }catch(err){
+
+    }
 }
--- main.js
+++ main.js
@@ -6,29 +6,34 @@ dotenv.config();
 const cors = require("cors");
 const path = require("path");
 const { spawn } = require('child_process');
-const mysql=require("mysql2");
-const { error } = require("console");
-const db=mysql.createConnection({
+const mysql = require("mysql2");
+const bcrypt = require("bcrypt");
+
+const db = mysql.createConnection({
     host: process.env.db_host,
     user: process.env.db_user,
     password: process.env.db_psw,
     database: "supercore_db"
 });
-db.connect((error)=>{
-    if(error){
-        console.error(`Erro ao conectar ao database: ${error}`)
-    } else{
-        console.log("Conectado om sucesso ao database.")
+
+db.connect((error) => {
+    if (error) {
+        console.error(`Erro ao conectar ao database: ${error}`);
+    } else {
+        console.log("Conectado com sucesso ao database.");
     }
-})
+});
+
 let PORT = process.env.PORT || 5001; 
 
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'client')));
-app.enable("trust proxy")
+app.enable("trust proxy");
+
 //================================ rotas api ===================================
+
 app.post('/chat', (req, res) => {
     const content = req.body.content;
     const modelo_ia = req.body.model;
@@ -37,7 +42,8 @@ app.post('/chat', (req, res) => {
     console.log("Pergunta recebida:", content);
     console.log("Modelo escolhido:", modelo_ia);
     console.log("===========================");
-    let ip=req.ip;
+    
+    let ip = req.ip;
     const processoPython = spawn('python3', ['main.py', content, modelo_ia, ip]);
 
     let respostaDaIA = "";
@@ -58,26 +64,35 @@ app.post('/chat', (req, res) => {
         }
     });
 });
-app.get("/", (req, res) =>{
+
+app.get("/", (req, res) => {
     res.sendStatus(200);
-})
-app.get("/chat" , (req, res) => {
+});
+
+app.get("/chat", (req, res) => {
     res.status(403).send("Only Post, if you are an normal user, please leave this route.");
 });
+
 app.post("/login", (req, res) => {
     const { user, password } = req.body;
-    const str = "SELECT * FROM Users WHERE Nome = ? AND Senha = ?";
+    
+    const str = "SELECT * FROM Users WHERE Nome = ?";
 
-    db.query(str, [user, password], (err, result) => {
+    db.query(str, [user], async (err, result) => {
         if (err) {
             console.error("Erro no banco:", err);
-            return res.sendStatus(403); 
+            return res.status(500).send("<strike>AAAA BROCACHO ITS THE END OF WORL-</strike>"); 
         }
-
         if (result.length === 0) {
             return res.status(401).json({ message: "Usuário ou senha incorretos" });
         }
+
         const usuarioLogado = result[0];
+        const senhaCorreta = await bcrypt.compare(password, usuarioLogado.Senha);
+
+        if (!senhaCorreta) {
+            return res.status(401).json({ message: "Usuário ou senha incorretos" });
+        }
 
         res.status(200).json({
             message: "Login efetuado com sucesso!",
@@ -89,22 +104,35 @@ app.post("/login", (req, res) => {
     });
 });
 
-app.post("/login/create", async (req, res) =>{
-    const {user,password}=req.body;
-    const sql=(`INSERT INTO Users (Nome, senha) VALUES (?,?)`);
-    db.query(sql, [user,password], (err, result)=>{
-        if(err){
-            console.error(`Erro ao criar novo usuário no banco de dados: ${err}`);
-            res.status(500).json({
-                message: "Ocorreu um erro no servidor."
-            })
-        }
-        if(result){
-            res.status(201).json({
-                message: "OK"
-            });
-        }
-    });
+app.post("/login/create", async (req, res) => {
+    const { user, password } = req.body;
+
+    try {
+        const salt = await bcrypt.genSalt(10);
+        const passwordHash = await bcrypt.hash(password, salt);
+
+        const sql = "INSERT INTO Users (Nome, Senha) VALUES (?,?)";
+        
+        db.query(sql, [user, passwordHash], (err, result) => {
+            if (err) {
+                console.error(`Erro ao criar novo usuário no banco de dados: ${err}`);
+                return res.status(500).json({
+                    message: "Ocorreu um erro no servidor ao criar o usuário."
+                });
+            }
+            
+            if (result) {
+                return res.status(201).json({
+                    message: "Usuário criado com sucesso!"
+                });
+            }
+        });
+    } catch (error) {
+        console.error("Erro ao gerar hash da senha:", error);
+        res.status(500).json({ message: "Erro interno no servidor." });
+    }
+});
+app.post("/get_hash", async (req, res) =>{
 
 })
 // =========================================================================
