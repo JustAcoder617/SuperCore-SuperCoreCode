8c6ce8c5eedee41b9ee3f60c4aab3424f285be36
Alysson <antonioalvesdasilvaalysson0@Gmail.com>
2026-06-09 21:55:41 -0300

    feat: adicionar elementos de entrada para nome de usuário e senha na página de login

---
+++ client/login_page/index.js
@@ -0,0 +1,6 @@
+const env_btn=document.getElementById("env");
+const username_input=document.getElementById("usr");
+const password_input=document.getElementById("psw");
+async function enviar_dados_ao_sv(data) {
+    
+}
